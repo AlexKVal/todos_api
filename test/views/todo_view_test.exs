b9f0744032830db2267e8c86102e107bd3bc7b47
AlexKVal <alexkval@gmail.com>
2016-10-02 22:25:56 +0300

    Add tests for view index.json and show.json

--- test/views/todo_view_test.exs
+++ test/views/todo_view_test.exs
@@ -1,6 +1,9 @@
 defmodule TodosApi.TodoViewTest do
   use TodosApi.ConnCase, async: true
 
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
   alias TodosApi.TodoView
 
   test "todo_json" do
@@ -13,6 +16,26 @@ defmodule TodosApi.TodoViewTest do
       description: todo.description,
       inserted_at: todo.inserted_at,
       updated_at: todo.updated_at
+    }
+  end
+
+  test "index.json" do
+    todo = insert(:todo)
+
+    rendered_todos = TodoView.render("index.json", %{todos: [todo]})
+
+    assert rendered_todos == %{
+      todos: [TodoView.todo_json(todo)]
+    }
+  end
+
+  test "show.json" do
+    todo = insert(:todo)
+
+    rendered_todo = TodoView.render("show.json", %{todo: todo})
+
+    assert rendered_todo == %{
+      todo: TodoView.todo_json(todo)
     }
   end
 end
